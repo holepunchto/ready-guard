3aaf4d9781bc8406f65f4d1ad094eb328305a1f0
Sean Zellmer <sean@lejeunerenard.com>
2026-06-04 06:48:07 -0500

    Add methods & properties to README with example (#2)
    * Add methods & properties to README with example
    
    * Add `destroy()` test
    
    * Lint test.js
    
    * Lint README.md
    
    * Remove warnings
    
    * Add test showing that `opened` can be set after `destroyed`
    
    * Clarify that `.exit()` only needs be called once to set `opened`

--- README.md
+++ README.md
@@ -12,7 +12,71 @@ npm install ready-guard
 
 ```js
 const ReadyGuard = require('ready-guard')
+
+class CountOnce {
+  constructor() {
+    this.value = 0
+    this.counting = new ReadyGuard()
+    this.count()
+  }
+
+  count() {
+    if (!this.counting.enter()) return this.counting.ready()
+
+    // Simulate doing something asynchronous
+    setTimeout(() => {
+      this.value++
+      this.counting.exit()
+    }, 100)
+    return this.counting.ready()
+  }
+}
+
+const counter = new CountOnce()
+await Promise.all([counter.count(), counter.count()])
+
+console.log(counter.value) // 1
 ```
+
+#### `const guard = new ReadyGuard()`
+
+Create a ready guard instance.
+
+#### `await guard.ready()`
+
+A promise representing the scope for the guard has been exited at least once.
+
+#### `const bool = guard.enter()`
+
+Called to note that the guarded process was entered or initiated. Useful for debouncing calls for readying something once:
+
+```js
+async function scope() {
+  if (!this.guard.enter()) return this.guard.ready()
+  // Do stuff...
+  this.guard.exit()
+}
+```
+
+#### `guard.exit()`
+
+Mark leaving the scope that was guarded. Resolves the `ready()` promise when called.
+
+#### `guard.destroy()`
+
+Cancels the scope, marks the guard as destroyed and rejects the ready promise.
+
+#### `guard.entered`
+
+Whether the scope was entered via `guard.enter()`.
+
+#### `guard.opened`
+
+Whether the scope completed via `guard.exit()` at least once.
+
+#### `guard.destroyed`
+
+Whether the guard was destroyed via `guard.destroy()`.
 
 ## License
 
--- test.js
+++ test.js
@@ -18,3 +18,61 @@ test('basic', async function (t) {
     b.exit()
   }
 })
+
+test('readme - example', async (t) => {
+  class CountOnce {
+    constructor() {
+      this.value = 0
+      this.counting = new ReadyGuard()
+      this.count()
+    }
+
+    count() {
+      if (!this.counting.enter()) return this.counting.ready()
+
+      // Simulate doing something asynchronous
+      setTimeout(() => {
+        this.value++
+        this.counting.exit()
+      }, 100)
+      return this.counting.ready()
+    }
+  }
+
+  const counter = new CountOnce()
+  await Promise.all([counter.count(), counter.count()])
+
+  t.is(counter.value, 1, 'ran once')
+})
+
+test('opened - can be set true after destroy()', (t) => {
+  const b = new ReadyGuard()
+
+  t.absent(b.opened, 'initially not opened')
+  t.ok(b.enter(), 'first enter & before destroy returns true')
+  t.execution(b.destroy(), 'destroy() doesnt throw itself')
+  t.execution(b.exit(), 'exit() doesnt throw')
+  t.ok(b.opened, 'flagged as opened')
+  t.ok(b.destroyed, 'flagged as destroyed')
+
+  t.execution(b.exit(), 'exit() can be called again')
+
+  // Intentionally placed last to 1) catch the error 2) show that opened is set before checking ready
+  t.exception(() => b.ready(), 'Ready guard destroyed', 'throws after destroying')
+})
+
+test('destroy', (t) => {
+  const b = new ReadyGuard()
+
+  t.absent(b.destroyed, 'destroyed flag initially not set')
+  t.ok(b.enter(), 'first enter & before destroy returns true')
+  t.execution(b.destroy(), 'destroy() doesnt throw itself')
+  t.exception(() => b.ready(), 'Ready guard destroyed', 'throws after destroying')
+  t.ok(b.destroyed, 'sets flag as destroyed')
+
+  const b2 = new ReadyGuard()
+
+  b2.destroy()
+  t.exception(() => b2.ready(), 'Ready guard destroyed', 'throws after destroying again')
+  t.absent(b2.enter(), 'enter() after destroy() returns false')
+})
